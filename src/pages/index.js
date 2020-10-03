86847c073105c39115e0b733b96467822e0cffa7
Rich Powell <rich@wearecore.co.uk>
2020-10-03 22:08:48 +0100

    Tweak FontAwesome

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 // import { Link } from "gatsby"
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faFacebook, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { faMobileAlt, faChevronDown } from '@fortawesome/free-solid-svg-icons'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -9,7 +9,7 @@ import Projects from "../components/projects"
 import Hero from "../components/hero"
 import HeroBG from "../images/digital-world-2.jpg"
 
-library.add(fab, faMobileAlt, faChevronDown)
+library.add(faFacebook, faLinkedin, faMobileAlt, faChevronDown)
 
 const IndexPage = () => (
   <Layout>
